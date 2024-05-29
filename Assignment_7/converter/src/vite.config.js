8f5162cb55376e714fbfe7ae9ed4b7a127ea7958
Navaneetha-Krishnan-S <krish.cgac@gmail.com>
2024-05-29 08:12:48 +0530

    .

---
+++ Assignment_7/converter/src/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/Vario_Tech_MERN_Assignments/Assignment_7/converter/',
+  plugins: [react()],
+});
